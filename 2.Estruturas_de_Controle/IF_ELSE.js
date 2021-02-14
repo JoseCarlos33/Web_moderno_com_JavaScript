693934cf6b29a8c10458387d4bd76578d0b4473e
José Carlos Noronha <67120200+JoseCarlos33@users.noreply.github.com>
2021-02-14 13:00:58 -0300

    Add files via upload

---
+++ 2.Estruturas_de_Controle/IF_ELSE.js
@@ -0,0 +1,11 @@
+function imprimirResultado(nota){
+    if(nota>=7){
+        console.log('PARABÉNS! Você foi APROVADO!')
+    }else{
+        console.log('Reprovado')
+    }
+}
+
+imprimirResultado(7.0)
+imprimirResultado(6.9)
+imprimirResultado(8.1)
