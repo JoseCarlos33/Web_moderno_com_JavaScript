4147d0fd182b6ec8d2e166ce315f2d7733cd345e
José Carlos Noronha <67120200+JoseCarlos33@users.noreply.github.com>
2021-03-01 17:32:29 -0300

    Add files via upload

--- 6.NodeJs/projeto/src/servidor.js
+++ 6.NodeJs/projeto/src/servidor.js
@@ -4,7 +4,19 @@ const express = require('express')
 const app = express()
 
 app.get('/produtos', (request, response, next)=>{
-    response.send({nome:'Notebook', preco: 123.45})
+    response.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id',(req, res)=>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, ()=>{
